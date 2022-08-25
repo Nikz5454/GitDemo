b30f05f0aeefdb4f0cbce45f2141ae0199950fc6
omkar <maliomkar90@gmail.com>
2022-08-25 19:31:32 +0530

    Initial Commit in HelloWorld by Omkar-Mali

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+
+		System.out.println("Hello World by Omkar");
+		System.out.println("Hello World to Nikhil");
+		System.out.println("Hello World by Mahesh");
+		System.out.println("Hello World by Shubham");
 	}
 
 }
