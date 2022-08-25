7d6b3e42a20b953dc648236690918abfe12d76cd
mahesh <hp@DESKTOP>
2022-08-25 20:16:44 +0530

    hello world commited by mahesh

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("mala nikhi n omkar chya repo. madhe add kel nahi");
 	}
 
 }
